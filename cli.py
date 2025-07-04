217c06284908836aadb9f38181e5756cb6a2d3b4
Daniel Baughman <danielrbaughman@icloud.com>
2025-07-04 10:52:01 -0400

    major refactor

--- cli.py
+++ cli.py
@@ -1,12 +1,29 @@
 import typer
 from rich import print
 
+from airtableformulahelpers import AND, NOT, BooleanField, DateField, TextField, TextListField
+
 app = typer.Typer(rich_markup_mode="markdown")
 
 
 @app.command()
 def main():
-    print("Hello, World!")
+    formula = AND(
+        "field1",
+        TextField(name="field2").contains("value2"),
+        NOT(
+            TextField(name="field3").starts_with("value3"),
+            TextField(name="field4").not_starts_with("value4"),
+            TextField(name="field5").not_equals("value5"),
+            TextField(name="field6").is_empty(),
+            TextField(name="field7").contains("value7"),
+            TextField(name="field8").not_contains("value8"),
+            TextListField(name="field9").contains("value9"),
+            BooleanField(name="field10").is_true(),
+            DateField(name="field11").is_on().value("2023-10-01"),
+        ),
+    )
+    print(formula)
 
 
 if __name__ == "__main__":
--- src/airtableformulahelpers/__init__.py
+++ src/airtableformulahelpers/__init__.py
@@ -1,236 +1,215 @@
+from typing import Literal
+
 import dateparser
+from pydantic import BaseModel
+
+# class Formula():
+#     table: str
+#     # use_field_ids: bool
+#     # field_id_map: dict[str, str]
+
+#     def __init__(self, table: str):
+#         self.table = table
 
 
-class DateComparison:
-    compare: str
+def AND(*args: str) -> str:  # noqa: N802
+    return f"AND({','.join(args)})"
 
-    def __init__(self, compare: str):
-        self.compare = compare
 
-    def value(self, field: str, date: str) -> str:
+def OR(*args: str) -> str:  # noqa: N802
+    return f"OR({','.join(args)})"
+
+
+def XOR(*args: str) -> str:  # noqa: N802
+    return f"XOR({','.join(args)})"
+
+
+def NOT(*args: str) -> str:  # noqa: N802
+    return f"NOT({','.join(args)})"
+
+
+def IF(condition: str, true_value: str, false_value: str) -> str:  # noqa: N802
+    return f"IF({condition}, {true_value}, {false_value})"
+
+
+
+
+def id_equals(id: str) -> str:
+    return f"RECORD_ID()='{id}'"
+
+class Field(BaseModel):
+    name: str
+
+    def is_empty(self) -> str:
+        return f"{{{self.name}}}=BLANK()"
+
+class TextField(Field):
+    """String comparison formulas"""
+
+    def equals(self, value: str) -> str:
+        return f'{{{self.name}}}="{value}"'
+
+    def not_equals(self, value: str) -> str:
+        return f'{{{self.name}}}!="{value}"'
+
+    def _find(self, value: str, comparison: str) -> str:
+        """case-insensitive"""
+        return f'FIND(TRIM(LOWER("{value}")), TRIM(LOWER({{{self.name}}}))){comparison}'
+
+    def contains(self, value: str) -> str:
+        """case-insensitive"""
+        return self._find(value, ">0")
+
+    def not_contains(self, value: str) -> str:
+        """case-insensitive"""
+        return self._find(value, "=0")
+
+    def starts_with(self, value: str) -> str:
+        """case-insensitive"""
+        return self._find(value, "=1")
+
+    def not_starts_with(self, value: str) -> str:
+        """case-insensitive"""
+        return self._find(value, "!=1")
+
+    def _ends_with(self, value: str, comparison: str) -> str:
+        """case-insensitive"""
+        return f'FIND(TRIM(LOWER("{value}")), TRIM(LOWER({{{self.name}}}))) {comparison} LEN(TRIM(LOWER({{{self.name}}}))) - LEN(TRIM(LOWER("{value}"))) + 1'
+
+    def ends_with(self, value: str) -> str:
+        """case-insensitive"""
+        return self._ends_with(value, "=")
+
+    def not_ends_with(self, value: str) -> str:
+        """case-insensitive"""
+        return self._ends_with(value, "!=")
+
+    def regex_match(self, pattern: str) -> str:
+        return f'REGEX_MATCH({{{self.name}}}, "{pattern}")'
+
+
+class TextListField(Field):
+    """List comparison formulas"""
+
+    def contains(self, value: str) -> str:
+        return f'FIND(LOWER("{value}"), LOWER({{{self.name}}}))>0'
+
+    def not_contains(self, value: str) -> str:
+        return f'FIND(LOWER("{value}"), LOWER({{{self.name}}}))=0'
+
+    def contains_all(self, values: list[str]) -> str:
+        return AND(*[self.contains(value) for value in values])
+
+    def contains_any(self, values: list[str]) -> str:
+        return OR(*[self.contains(value) for value in values])
+
+COMPARISON = Literal["=", "!=", ">", "<", ">=", "<="]
+
+class Number(Field):
+    """Number comparison formulas"""
+
+    def _compare(self, comparison: COMPARISON, value: int | float) -> str:
+        return f"{{{self.name}}}{comparison}{value}"
+
+    def equals(self, value: int | float) -> str:
+        return self._compare("=", value)
+
+    def not_equals(self, value: int | float) -> str:
+        return self._compare("!=", value)
+
+    def greater_than(self, value: int | float) -> str:
+        return self._compare(">", value)
+
+    def less_than(self, value: int | float) -> str:
+        return self._compare("<", value)
+
+    def greater_than_or_equals(self, value: int | float) -> str:
+        return self._compare(">=", value)
+
+    def less_than_or_equals(self, value: int | float) -> str:
+        return self._compare("<=", value)
+
+
+class BooleanField(Field):
+    """Boolean comparison formulas"""
+
+    def equals(self, value: bool) -> str:
+        return f"{{{self.name}}}={'TRUE()' if value else 'FALSE()'}"
+
+    def is_true(self) -> str:
+        return f"{{{self.name}}}=TRUE()"
+
+    def is_false(self) -> str:
+        return f"{{{self.name}}}=FALSE()"
+
+
+class AttachmentsField(Field):
+    """Attachment comparison formulas"""
+
+    def is_not_empty(self) -> str:
+        return f"LEN({{{self.name}}})>0"
+
+    def is_empty(self) -> str:
+        return f"LEN({{{self.name}}})=0"
+
+    def count_is(self, count: int) -> str:
+        return f"LEN({{{self.name}}})={count}"
+
+
+class DateComparison(Field):
+    compare: COMPARISON
+
+    def value(self, date: str) -> str:
         parsed_date = dateparser.parse(date)
-        return f"DATETIME_PARSE('{parsed_date}'){self.compare}DATETIME_PARSE({{{field}}})"
+        return f"DATETIME_PARSE('{parsed_date}'){self.compare}DATETIME_PARSE({{{self.name}}})"
 
-    def _ago(self, field: str, unit: str, value: int) -> str:
-        return f"DATETIME_DIFF(NOW(), {{{field}}}, '{unit}'){self.compare}{value}"
+    def _ago(self, unit: str, value: int) -> str:
+        return f"DATETIME_DIFF(NOW(), {{{self.name}}}, '{unit}'){self.compare}{value}"
 
-    def milliseconds_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "milliseconds", value)
+    def milliseconds_ago(self, value: int) -> str:
+        return self._ago("milliseconds", value)
 
-    def seconds_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "seconds", value)
+    def seconds_ago(self, value: int) -> str:
+        return self._ago("seconds", value)
 
-    def minutes_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "minutes", value)
+    def minutes_ago(self, value: int) -> str:
+        return self._ago("minutes", value)
 
-    def hours_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "hours", value)
+    def hours_ago(self, value: int) -> str:
+        return self._ago("hours", value)
 
-    def days_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "days", value)
+    def days_ago(self, value: int) -> str:
+        return self._ago("days", value)
 
-    def weeks_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "weeks", value)
+    def weeks_ago(self, value: int) -> str:
+        return self._ago("weeks", value)
 
-    def months_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "months", value)
+    def months_ago(self, value: int) -> str:
+        return self._ago("months", value)
 
-    def quarters_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "quarters", value)
+    def quarters_ago(self, value: int) -> str:
+        return self._ago("quarters", value)
 
-    def years_ago(self, field: str, value: int) -> str:
-        return self._ago(field, "years", value)
+    def years_ago(self, value: int) -> str:
+        return self._ago("years", value)
 
+class DateField(Field):
+    """DateTime comparison formulas"""
 
-class ATFormula:
-    """Abstracts Airtable formula syntax for easier use"""
+    def is_on(self) -> DateComparison:
+        return DateComparison(name=self.name, compare="=")
+    
+    def is_on_or_after(self) -> DateComparison:
+        return DateComparison(name=self.name, compare=">=")
 
-    @staticmethod
-    def id_equals(id: str) -> str:
-        return f"RECORD_ID()='{id}'"
+    def is_on_or_before(self) -> DateComparison:
+        return DateComparison(name=self.name, compare="<=")
 
-    @staticmethod
-    def is_empty(field: str) -> str:
-        return f"{{{field}}}=BLANK()"
+    def is_after(self) -> DateComparison:
+        return DateComparison(name=self.name, compare="<")
+    
+    def is_before(self) -> DateComparison:
+        return DateComparison(name=self.name, compare=">")
 
-    class Logic:
-        """AND, OR, XOR, NOT, IF"""
-
-        @staticmethod
-        def AND(*args: str) -> str:  # noqa: N802
-            return f"AND({','.join(args)})"
-
-        @staticmethod
-        def OR(*args: str) -> str:  # noqa: N802
-            return f"OR({','.join(args)})"
-
-        @staticmethod
-        def XOR(*args: str) -> str:  # noqa: N802
-            return f"XOR({','.join(args)})"
-
-        @staticmethod
-        def NOT(*args: str) -> str:  # noqa: N802
-            return f"NOT({','.join(args)})"
-
-        @staticmethod
-        def IF(condition: str, true_value: str, false_value: str) -> str:  # noqa: N802
-            """IF(condition, true_value, false_value)"""
-            return f"IF({condition}, {true_value}, {false_value})"
-
-    class String:
-        """String comparison formulas"""
-
-        @staticmethod
-        def equals(field: str, value: str) -> str:
-            return f'{{{field}}}="{value}"'
-
-        @staticmethod
-        def not_equals(field: str, value: str) -> str:
-            return f'{{{field}}}!="{value}"'
-
-        @staticmethod
-        def _find(field: str, value: str, comparison: str) -> str:
-            return f'FIND(TRIM(LOWER("{value}")), TRIM(LOWER({{{field}}}))){comparison}'
-
-        @staticmethod
-        def contains(field: str, value: str) -> str:
-            return ATFormula.String._find(field, value, ">0")
-
-        @staticmethod
-        def not_contains(field: str, value: str) -> str:
-            return ATFormula.String._find(field, value, "=0")
-
-        @staticmethod
-        def starts_with(field: str, value: str) -> str:
-            return ATFormula.String._find(field, value, "=1")
-
-        @staticmethod
-        def not_starts_with(field: str, value: str) -> str:
-            return ATFormula.String._find(field, value, "!=1")
-
-        @staticmethod
-        def _ends_with(field: str, value: str, comparison: str) -> str:
-            return f'FIND(TRIM(LOWER("{value}")), TRIM(LOWER({{{field}}}))) {comparison} LEN(TRIM(LOWER({{{field}}}))) - LEN(TRIM(LOWER("{value}"))) + 1'
-
-        @staticmethod
-        def ends_with(field: str, value: str) -> str:
-            return ATFormula.String._ends_with(field, value, "=")
-
-        @staticmethod
-        def not_ends_with(field: str, value: str) -> str:
-            return ATFormula.String._ends_with(field, value, "!=")
-
-        @staticmethod
-        def regex_match(field: str, pattern: str) -> str:
-            return f'REGEX_MATCH({{{field}}}, "{pattern}")'
-
-    class StringList:
-        """List comparison formulas"""
-
-        @staticmethod
-        def contains(field: str, value: str) -> str:
-            return f'FIND(LOWER("{value}"), LOWER({{{field}}}))>0'
-
-        @staticmethod
-        def not_contains(field: str, value: str) -> str:
-            return f'FIND(LOWER("{value}"), LOWER({{{field}}}))=0'
-
-        @staticmethod
-        def contains_all(field: str, values: list[str]) -> str:
-            return ATFormula.Logic.AND(
-                *[ATFormula.StringList.contains(field, value) for value in values]
-            )
-
-        @staticmethod
-        def contains_any(field: str, values: list[str]) -> str:
-            return ATFormula.Logic.OR(
-                *[ATFormula.StringList.contains(field, value) for value in values]
-            )
-
-    class Number:
-        """Number comparison formulas"""
-
-        @staticmethod
-        def _compare(field: str, comparison: str, value: int | float) -> str:
-            return f"{{{field}}}{comparison}{value}"
-
-        @staticmethod
-        def equals(field: str, value: int | float) -> str:
-            return ATFormula.Number._compare(field, "=", value)
-
-        @staticmethod
-        def not_equals(field: str, value: int | float) -> str:
-            return ATFormula.Number._compare(field, "!=", value)
-
-        @staticmethod
-        def greater_than(field: str, value: int | float) -> str:
-            return ATFormula.Number._compare(field, ">", value)
-
-        @staticmethod
-        def less_than(field: str, value: int | float) -> str:
-            return ATFormula.Number._compare(field, "<", value)
-
-        @staticmethod
-        def greater_than_or_equals(field: str, value: int | float) -> str:
-            return ATFormula.Number._compare(field, ">=", value)
-
-        @staticmethod
-        def less_than_or_equals(field: str, value: int | float) -> str:
-            return ATFormula.Number._compare(field, "<=", value)
-
-    class Boolean:
-        """Boolean comparison formulas"""
-
-        @staticmethod
-        def equals(field: str, value: bool) -> str:
-            return f"{{{field}}}={'TRUE()' if value else 'FALSE()'}"
-
-        @staticmethod
-        def is_true(field: str) -> str:
-            return f"{{{field}}}=TRUE()"
-
-        @staticmethod
-        def is_false(field: str) -> str:
-            return f"{{{field}}}=FALSE()"
-
-    class Attachments:
-        """Attachment comparison formulas"""
-
-        @staticmethod
-        def exist(column_name: str) -> str:
-            return f"LEN({{{column_name}}})>0"
-
-        @staticmethod
-        def not_exist(column_name: str) -> str:
-            return f"LEN({{{column_name}}})=0"
-
-        @staticmethod
-        def count_is(column_name: str, count: int) -> str:
-            return f"LEN({{{column_name}}})={count}"
-
-    class DateTime:
-        """DateTime comparison formulas"""
-
-        class Equals(DateComparison):
-            def __init__(self):
-                super().__init__("=")
-
-        class OnOrAfter(DateComparison):
-            def __init__(self):
-                super().__init__("<=")
-
-        class OnOrBefore(DateComparison):
-            def __init__(self):
-                super().__init__(">=")
-
-        class After(DateComparison):
-            def __init__(self):
-                super().__init__("<")
-
-        class Before(DateComparison):
-            def __init__(self):
-                super().__init__(">")
-
-        class NotEquals(DateComparison):
-            def __init__(self):
-                super().__init__("!=")
+    def is_not_on(self) -> DateComparison:
+        return DateComparison(name=self.name, compare="!=")
